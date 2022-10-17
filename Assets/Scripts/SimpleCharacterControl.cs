a9724e8c8d7f4eac9bfe54326b339f6335d9c886
abazatte <92862432+abazatte@users.noreply.github.com>
2022-10-17 18:04:51 +0200

    erstes Praktikum done

---
+++ Assets/Scripts/SimpleCharacterControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterControl : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 5f;
+
+    public float fallSpeed = 5f;
+
+    private Animator animator;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        startPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 input = Camera.main.transform.forward * Input.GetAxis("Vertical") + Camera.main.transform.right * Input.GetAxis("Horizontal");
+        input.y = 0;
+        input.Normalize();
+        rb.MovePosition(transform.position + input * Time.deltaTime * speed);
+        if (input.sqrMagnitude > 10e-10)
+        {
+            rb.MoveRotation(Quaternion.LookRotation(input));
+        }
+
+        animator.SetFloat("Speed", rb.velocity.magnitude / speed);
+
+        //hier diese Stelle überarbeiten, damit es richtig kollidiert.
+        //
+        RaycastHit hit;
+        if (!rb.SweepTest(Vector3.down, out hit))
+        {
+
+            animator.SetBool("Grounded", false);
+            rb.MovePosition(transform.position + Vector3.down * Time.deltaTime * fallSpeed);
+        }
+        else
+        {
+            TipToePlatform tipToePlatform = hit.collider.GetComponent<TipToePlatform>();
+            if (tipToePlatform != null)
+            {
+                tipToePlatform.CharacterTouches();
+            }
+            animator.SetBool("Grounded", true);
+        }
+
+        if (transform.position.y < -10)
+        {
+            ResetPosition();
+        }
+    }
+
+    private void ResetPosition()
+    {
+        transform.position = startPos;
+    }
+}
---
+++ Assets/Scripts/TipToeLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class TipToeLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab;
+    private int width = 10;
+    private int depth = 13;
+    private float gapbetween = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformPrefab.transform.localScale = new Vector3(1.2f,1,1.2f);
+        for (int i = 0; i < depth; i++)
+        {
+            Random random = new Random();
+            //für jede Reihe neu bestimmen 
+            for (int j = 0; j < width; j++)
+            {
+                GameObject gameObject = Instantiate(platformPrefab, new Vector3(-13.52f + (gapbetween * j), 0, 10 + (gapbetween * i)), Quaternion.identity);
+
+                gameObject.AddComponent(typeof (BoxCollider));
+                gameObject.AddComponent(typeof(TipToePlatform));
+                int rnd = random.Next(1,3);
+                print(rnd);
+                if (rnd % 2 == 0)
+                {
+                    gameObject.GetComponent<TipToePlatform>().isPath = true;
+                }
+                
+                //TipToePlatform tipToePlatform = new TipToePlatform();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
